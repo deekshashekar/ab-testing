dfedde8eb85c45c98869d1588693c7ab5524b9e7
deekshashekar <deeksha.shekar@antstack.io>
2025-05-25 17:34:57 +0530

    added comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,13 @@ import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = getLayout<LayoutProps>(Component)
-
   return (
     <Layout
       title="AB Testing with buckets"
       path="edge-middleware/ab-testing-simple"
     >
       <Component {...pageProps} />
+      // TODO: add utm parameters
       {process.env.NEXT_PUBLIC_GTM_ID && (
         <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
       )}
