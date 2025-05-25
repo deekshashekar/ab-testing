1f3eff3ec7f37564d9ea6c82dd67d6d07773f30a
deekshashekar <deeksha.shekar@antstack.io>
2025-05-25 16:34:50 +0530

    added GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app'
 import type { LayoutProps } from '@vercel/examples-ui/layout'
 import { getLayout } from '@vercel/examples-ui'
 import '@vercel/examples-ui/globals.css'
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = getLayout<LayoutProps>(Component)
@@ -12,6 +14,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       path="edge-middleware/ab-testing-simple"
     >
       <Component {...pageProps} />
+      {process.env.NEXT_PUBLIC_GTM_ID && (
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
+      )}
     </Layout>
   )
 }
